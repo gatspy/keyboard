509f446b7a5b412669ebe5ddd093231835ceecab
gatspy <gatspy.if@gmail.com>
2017-11-26 20:51:38 +0800

    add my customise hyper app

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,17 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  -- { 'a', 'Alfred 2' },            -- "A" for "Alfred"
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'c', '微信' },             -- "C for "Chat"
+  { 'd', 'Dash' }, -- "D" for "Dash!"
+  { 'e', 'Sublime Text' },              -- "E" for "Editor"
+  { 'f', 'Path Finder' },            -- "F" for "Path Finder"
+  { 'g', 'Airmail 3' },       -- "G" for "Airmail"
+  { 'm', 'MWeb' },             -- "m" for "MWeb"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
+  { 'v', 'Visual Studio Code' },             -- "V" for "Visual Studio Code"
+}
